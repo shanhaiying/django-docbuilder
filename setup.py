4dbaa4473acb0011229a2af465d68cfad23434e1
Alexandre Joseph <jexhson@alexandrejoseph.com>
2013-02-06 15:49:41 +0100

    Add setup

---
+++ setup.py
@@ -0,0 +1,19 @@
+setup(
+    name = 'docbuilder',
+    version = '0.1',
+    description = 'Build reStructuredText to PDF (LaTeX) docs from a Django app ',
+    keywords = 'django pdf rst latex',
+    license = 'GNU LESSER GENERAL PUBLIC LICENSE',
+    url = 'http://github.com/jexhson/django-docbuilder',
+    dependency_links = [],
+    classifiers=[
+            'Development Status :: 4 - Beta',
+            'Environment :: Plugins',
+            'Framework :: Django',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: BSD License',
+            'Programming Language :: Python',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+    packages = ['docbuilder'],
+)
